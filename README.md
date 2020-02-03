e2c0674381e82cc9fdb30928d57af4e6992c9c0c
ilya <ilyakodit@mail.ru>
2020-02-03 18:37:38 +0300

    readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cake-store
 Проект сайта по продаже тортов
+
+![Image alt](https://github.com/ilyaKodit/cake-store/raw/master/imageReadme/cake-Store.jpg)
--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const hbs = require('hbs');
 
 const bodyParser = require('body-parser');
 
-const port =  process.env.PORT || 3000
+const port =  process.env.PORT || 3000;
 
 const app = express();
 
